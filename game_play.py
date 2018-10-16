015a6b22bc96e6e92c12ab5ada701db0941eecf2
Hyoung Sik "Harry" Won <32139488+wonhs@users.noreply.github.com>
2018-10-16 00:10:07 -0700

    start_narration moved to narration.py
    for simplification purpose

--- game_play.py
+++ game_play.py
@@ -21,25 +21,6 @@ def clear():
 
 
 #Game Start Narration
-title_narration_part1 = "Agent Dope, who is tasked with the mission of finding and appending " \
-                  "Dr. Crime – a once renowned scientist from the affluent Crime family, " \
-                  "known for their charity and philanthropy. After spending nearly a decade " \
-                  "working on the theory of Dark Magic to restore Life, Dr. Crime’s work was " \
-                  "met with great criticism and disapproval from the scientific community, and " \
-                  "was ridiculed for such preposterous ideas. Dr. Crime went down a terrible " \
-                  "spiral and started to use Dark Magic for mischief and evil. He vowed to " \
-                  "take vengeance on the citizens of the city of New San Diego by using Dark " \
-                  "Magic to poison the city and kill all of its citizens. "
-
-title_narration_part2 = "The latest reconnaissance shows that Dr. Crime has established a lair " \
-                    "which is protected by a Magical Barrier, which can only be destroyed by " \
-                    "an individual who possess the Magical Orbs. The reconnaissance also revealed " \
-                    "that Dr. Crime is clothed in a Magical Armor that can be only penetrated by " \
-                    "the Magic Sword of Light. With none of these in hand, the cautious, yet " \
-                    "courageous Agt. Dope must explore the city of New San Diego, find all the " \
-                    "necessary artifacts, and use them to stop Dr. Crime before the poison is " \
-                    "released. Hurry, time is ticking!"
-
 short_for_agt_dope = "An elite secret agent who's after Dr. Crime" 
 
 long_for_agt_dope  = "An elite secret agent who is an expert in forensic and many other scientific " \
@@ -57,14 +38,14 @@ def game_play(ga: gameaction.GameAction):
     clear()
 
     main_menu.dotted_line(main_menu.GAME_WIDTH)
-    narration.narration(title_narration_part1, main_menu.GAME_WIDTH)
+    narration.narration(narration.start_narration1(), main_menu.GAME_WIDTH)
     main_menu.dotted_line(main_menu.GAME_WIDTH)
     input("\nPress any key to continue...\n")
 
     clear()
 
     main_menu.dotted_line(main_menu.GAME_WIDTH)
-    narration.narration(title_narration_part2, main_menu.GAME_WIDTH)
+    narration.narration(narration.start_narration2(), main_menu.GAME_WIDTH)
     main_menu.dotted_line(main_menu.GAME_WIDTH)
     input("\nPress any key to continue...\n")
     
