b87780ad85b404da4fa73f45d7d054ce98e3b45f
Hyoung Sik "Harry" Won <32139488+wonhs@users.noreply.github.com>
2018-10-16 00:09:30 -0700

    start narrations moved here

--- narration.py
+++ narration.py
@@ -1,3 +1,30 @@
+def start_narration1():
+	title_narration_part1 = "Agent Dope, who is tasked with the mission of finding and appending " \
+                  "Dr. Crime – a once renowned scientist from the affluent Crime family, " \
+                  "known for their charity and philanthropy. After spending nearly a decade " \
+                  "working on the theory of Dark Magic to restore Life, Dr. Crime’s work was " \
+                  "met with great criticism and disapproval from the scientific community, and " \
+                  "was ridiculed for such preposterous ideas. Dr. Crime went down a terrible " \
+                  "spiral and started to use Dark Magic for mischief and evil. He vowed to " \
+                  "take vengeance on the citizens of the city of New San Diego by using Dark " \
+                  "Magic to poison the city and kill all of its citizens. "
+	
+	return title_narration_part1
+	
+	
+def start_narration2():
+	title_narration_part2 = "The latest reconnaissance shows that Dr. Crime has established a lair " \
+                    "which is protected by a Magical Barrier, which can only be destroyed by " \
+                    "an individual who possess the Magical Orbs. The reconnaissance also revealed " \
+                    "that Dr. Crime is clothed in a Magical Armor that can be only penetrated by " \
+                    "the Magic Sword of Light. With none of these in hand, the cautious, yet " \
+                    "courageous Agt. Dope must explore the city of New San Diego, find all the " \
+                    "necessary artifacts, and use them to stop Dr. Crime before the poison is " \
+                    "released. Hurry, time is ticking!"
+	
+	return title_narration_part2
+
+
 def narration(the_string, line_length):
     counter = 0
 
