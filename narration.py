9821b59d17938bdbae2bb1c5bd85037351f4d78c
Hyoung Sik "Harry" Won <32139488+wonhs@users.noreply.github.com>
2018-10-19 23:58:29 -0700

    help added

--- narration.py
+++ narration.py
@@ -6,6 +6,7 @@ def possible_menu():
         main_menu.print_in_the_middle(main_menu.GAME_WIDTH, "- Open Inventory                    ")
         main_menu.print_in_the_middle(main_menu.GAME_WIDTH, "- Interact with the district feature")
         main_menu.print_in_the_middle(main_menu.GAME_WIDTH, "- Exit to the main menu             ")
+        main_menu.print_in_the_middle(main_menu.GAME_WIDTH, "- Help                              ")
 
 def start_narration1():
 	title_narration_part1 = "Agent Dope, who is tasked with the mission of finding and appending " \
