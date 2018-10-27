a790924fd71e7705198f492b655ee47b39174096
Hyoung Sik "Harry" Won <32139488+wonhs@users.noreply.github.com>
2018-10-27 15:34:56 -0700

    Update stub_map.py

--- stub_map.py
+++ stub_map.py
@@ -3,40 +3,14 @@ import city
 from items import Item, ItemType, Action, ActionType
 
 from typing import List, Dict
+import item_instances
 
 def get_map_stub():
     city_hall = city.District(city.DistrictId(2, 3),
                               "City Hall",
                               [
-                                  Item("Statue",
-                                     ItemType.FEATURE,
-                                     "A Statue sits in the center of the park.",
-                                     "A giant bronze statue of the city's founder. It's reflecting the sun's blinding light.",
-                                     [
-                                           Action(["view the statue", "look at the statue"], ActionType.DISPLAY, "A giant bronze statue of the city's founder. It's reflecting the sun's blinding light."),
-                                           Action(["eat statue"], ActionType.DISPLAY, "Don't be silly - you can't eat that.")
-                                       ]
-                                  ),
-                                  Item("Lotto Ticket",
-                                       ItemType.CRITICAL,
-                                       "You see a Lotto Ticket on the ground.",
-                                       "A Lotto Ticket with the numbers '23 57 12' on it.",
-                                       [
-                                           Action(["view the lotto ticket", 
-                                           "look at the lotto ticket", 
-                                           "view the lotto", 
-                                           "look at the lotto", 
-                                           "look at the ticket"], ActionType.DISPLAY, "A Lotto Ticket with the numbers '23 57 12' on it."),
-                                           Action(["eat the lotto", 
-                                           "eat the lotto ticket"], ActionType.DISPLAY, "There must be a better way to get fiber in your system."),
-                                           Action(["take the lotto", 
-                                           "pick up the lotto",
-                                           "take the ticket",
-                                           "pick up the ticket",
-                                           "pick up the lotto ticket",
-                                           "take the lotto ticket"], ActionType.ACTION, "The lotto ticket is in my inventory now.", ActionType.TAKE_ITEM)
-                                       ]
-                                  )
+                                  item_instances.statue,
+                                  item_instances.lotto_ticket
                               ], # district_items
                               [], # dropped_items
                               [], # clues
