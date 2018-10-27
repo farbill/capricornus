ffa0bc1b0f0b6c84f3c2dac7d67d430c6affded4
Hyoung Sik "Harry" Won <32139488+wonhs@users.noreply.github.com>
2018-10-26 23:16:09 -0700

    look removed

--- main_menu.py
+++ main_menu.py
@@ -228,7 +228,6 @@ def gameplay_selection(the_input: str,
         ["exit", "quit", "q", "exit game", "quit game", "bye", "byebye", "main", "main menu"],
         ["inventory", "bag", "open inventory", "open bag", "see inventory", "check inventory"],
         ["help", "help me", "manual", "guide", "q&a", "open help"],
-        ["look", "look around", "inspect", "see", "check"],
         # ....
     ]
 
