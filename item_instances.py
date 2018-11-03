d35872f8ef8930046891a64710a8b004e02180ab
Hyoung Sik "Harry" Won <32139488+wonhs@users.noreply.github.com>
2018-11-02 19:51:00 -0700

    vit orb added

--- item_instances.py
+++ item_instances.py
@@ -7,4 +7,4 @@ old_siver_coin = ItemLoader.parse_json('etc/items/old_silver_coin.json')
 #LEGENDARY ITEMS
 magic_sword = ItemLoader.parse_json('etc/items/magic_sword.json')
 vision_orb = ItemLoader.parse_json('etc/items/vision_orb.json')
-
+strength_orb = ItemLoader.parse_json('etc/items/strength_orb.json')
