6e63e0231130f219113bddfaec1134a6fabaa196
farbill <wfargo@lhft.io>
2018-11-20 21:19:48 +0800

    nlp

--- command_parsing.py
+++ command_parsing.py
@@ -1,6 +1,10 @@
+from nlp import NLP
+translator = NLP()
+
+
 def command_parsing(command, allowed_list):
     for i in allowed_list:
-        if command == i:
+        if translator.translate(command) == i:
             return 1
 
     return 0
